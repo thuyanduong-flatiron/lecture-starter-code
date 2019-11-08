d0eba2802ea193dba61468ecb235a3fa04f3f211
Ann <thuyan.duong@flatironschool.com>
2019-11-07 19:40:51 -0500

    updated 1-8

--- Mod1/08-Intro-To-Active-Record/lib/user.rb
+++ Mod1/08-Intro-To-Active-Record/lib/user.rb
@@ -1,56 +1,38 @@
 class User
-  attr_accessor :name, :id
+  attr_accessor :username, :id
 
-  def initialize(name, id=nil)
+  def initialize(username, id=nil)
     @name = name
     @id = id
   end
 
+  def self.welcome
+    puts "Welcome to Twitter"
+  end
+
   def self.create(name)
-    author = Author.new(name)
-    create_author_sql = <<-SQL
-      INSERT INTO authors (name) VALUES (?)
+    user = User.new(name)
+    create_user_sql = <<-SQL
+      INSERT INTO user (username) VALUES (?)
     SQL
-    DB[:conn].execute(create_author_sql, name)
+    DB[:conn].execute(create_user_sql, name)
     new_id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
-    author.id = new_id
-    author
+    user.id = new_id
+    user
   end
 
   def self.all
     get_all_sql = <<-SQL
-      SELECT * FROM authors
+      SELECT * FROM users
     SQL
-    db_authors_array = DB[:conn].execute(get_all_sql)
-    db_authors_array.map{ |row|
+    db_users_array = DB[:conn].execute(get_all_sql)
+    db_users_array.map{ |row|
       self.make_instance_from_row(row)
     }
   end
 
   def self.make_instance_from_row(row)
-    Author.new(row[1], row[0])
+    User.new(row[1], row[0])
   end
 
-  def self.find(id)
-    find_author_sql = <<-SQL
-      SELECT * FROM authors WHERE id = ?
-    SQL
-    row = DB[:conn].execute(find_author_sql, id)[0]
-    self.make_instance_from_row(row)
-  end
-
-  def save
-    update_sql = <<-SQL
-      UPDATE authors SET name = ? WHERE id = ?
-    SQL
-    DB[:conn].execute(update_sql, self.name, self.id)
-    self
-  end
-
-  def delete
-    delete_sql = <<-SQL
-      DELETE FROM authors WHERE id = ?
-    SQL
-    DB[:conn].execute(delete_sql, self.id)
-  end
 end
