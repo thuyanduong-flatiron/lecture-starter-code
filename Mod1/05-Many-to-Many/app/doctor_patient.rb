9fbfc60ca511e3a33612a1bb2c55adfa570dda77
Thuyan Duong <thuyan.duong@flatironschool.com>
2020-01-30 11:22:19 -0500

    update

--- Mod1/05-Many-to-Many/app/doctor_patient.rb
+++ Mod1/05-Many-to-Many/app/doctor_patient.rb
@@ -1,13 +1,5 @@
 require "pry"
 
 class DoctorPatient
-  attr_accessor :doctor, :patient 
-  @@all = []
-
-  def initialize(doctor, patient)
-    @doctor = doctor 
-    @patient = patient 
-    @@all << self
-  end 
 
 end 
