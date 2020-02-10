d44927957ae01b080992d51b3dbfd3fa8595595f
Thuyan Duong <thuyan.duong@flatironschool.com>
2020-02-09 21:51:37 -0500

    updated mod 1 api

--- Mod1/09-Hashes-and-the-Internet/bin/run.rb
+++ Mod1/09-Hashes-and-the-Internet/bin/run.rb
@@ -5,3 +5,9 @@ require 'awesome_print'
 URL = "https://www.googleapis.com/books/v1/volumes?q=ruby"
 response = RestClient.get(URL)
 p JSON.parse(response)
+
+# Welcome the user to our app with instructions
+# Get a search term as an input from the user
+# Make an internet GET request to the GoogleBooksAPI with that search term
+# Parse the reponse from the server into a hash we can work with
+# Print out the first 10 books title and author
